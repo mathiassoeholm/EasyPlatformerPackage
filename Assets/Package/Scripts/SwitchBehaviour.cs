39e2aebf9832de9ffba56d7b4d8d290112077ba2
Mathias <mathiassoeholm@gmail.com>
2012-08-08 00:13:32 +0200

    Added more comments for documentation

--- Assets/Package/Scripts/SwitchBehaviour.cs
+++ Assets/Package/Scripts/SwitchBehaviour.cs
@@ -7,23 +7,24 @@ public class SwitchBehaviour : MonoBehaviour
     public int switchId;
 
     // A static list of all toggle objects in the scene
-    public static List<ToggleBehaviour> toggleObjects = new List<ToggleBehaviour>(); 
-    
+    public static List<ToggleBehaviour> toggleObjects = new List<ToggleBehaviour>();
 
+    // Awake is called once and before Start()
     public void Awake()
     {
+        // Get a reference to the TriggerToggle animation state
         AnimationState anim = transform.GetChild(0).animation["TriggerToggle"];
-        if (anim)
-        {
-            anim.speed *= -1;
-        }
+        
+        // Make the animation go in reverse to start with
+        anim.speed *= -1;
     }
 
-    // Character is calling it 
+    // Searches for associated objects, and call their Toggle method
     public void SearcForTriggeredObjects()
     {
         for (int i = toggleObjects.Count - 1; i >= 0; i--)
         {
+            // Check if id's match, and call Toggle() if true
             if(switchId == toggleObjects[i].switchID)
                 toggleObjects[i].Toggle();
         }
--- Assets/Package/Scripts/ToggleBehaviour.cs
+++ Assets/Package/Scripts/ToggleBehaviour.cs
@@ -3,21 +3,29 @@ using System.Collections;
 
 public abstract class ToggleBehaviour : MonoBehaviour
 {
+    // Switch ID, used to asscoiate with correct switch, assigned in the inspecter
     public int switchID = -1;
 
+    // The Start method heres is protected so it can be called from in derived class
     protected void Start()
     {
+        // Check if the swicth ID is bigger than or equal to 0, to check if user changed it. Default is -1, which has no effect
         if(switchID >= 0)
         {
+            // Add this toggle object to the static list
             SwitchBehaviour.toggleObjects.Add(this);
+
+            // Write a clarification message to the console
             Debug.Log("Added " + gameObject.name + " with trigger id " + switchID);
         } 
     }
 
     public void OnDestroy()
     {
+        // Remove this object from the list of toggle object in case it gets destroyed (Explosions get destroyed, and are toggle objects)
         SwitchBehaviour.toggleObjects.Remove(this);
     }
 
+    // Supply an abstract Toggle() method, that every derived class need to implement
     public abstract void Toggle();
 }
