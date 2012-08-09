77a0824b3bed56d80f65f0d6fe830d8b6d31ce08
Mathias Soeholm <mathiassoeholm@gmail.com>
2012-08-09 13:14:49 +0200

    Adding untracked new files, that didn't make the last commit

---
+++ Assets/Package/Scripts/YouWinScene.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class YouWinScene : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update ()
+    {
+	    // Check if we press enter
+        if(Input.GetKeyDown("return"))
+	    {
+	        // Load the first assigned scene in the build settings
+            Application.LoadLevel(0);
+	    }
+	}
+}
